a76ddfe4c6d3d6b39b2a730399d9d6ba69203904
GermanBluefox <dogafox@gmail.com>
2019-07-11 19:37:46 +0200

    (bluefox) initial commit

---
+++ widgets/devices/js/devices.js
@@ -0,0 +1,54 @@
+/*
+    ioBroker.vis devices Widget-Set
+
+    version: "0.0.1"
+
+    Copyright 2019 bluefox dogafox@gmail.com
+*/
+'use strict';
+
+// add translations for edit mode
+$.get( 'adapter/devices/words.js', function(script) {
+    let translation = script.substring(script.indexOf('{'), script.length);
+    translation = translation.substring(0, translation.lastIndexOf(';'));
+    $.extend(systemDictionary, JSON.parse(translation));
+});
+
+// this code can be placed directly in devices.html
+vis.binds['devices'] = {
+    version: '0.0.1',
+    showVersion: function () {
+        if (vis.binds['devices'].version) {
+            console.log('Version devices: ' + vis.binds['devices'].version);
+            vis.binds['devices'].version = null;
+        }
+    },
+    createWidget: function (widgetID, view, data, style) {
+        var $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function () {
+                vis.binds['devices'].createWidget(widgetID, view, data, style);
+            }, 100);
+        }
+
+        var text = '';
+        text += 'OID: ' + data.oid + '</div><br>';
+        text += 'OID value: <span class="myset-value">' + vis.states[data.oid + '.val'] + '</span><br>';
+        text += 'Color: <span style="color: ' + data.myColor + '">' + data.myColor + '</span><br>';
+        text += 'extraAttr: ' + data.extraAttr + '<br>';
+        text += 'Browser instance: ' + vis.instance + '<br>';
+        text += 'htmlText: <textarea readonly style="width:100%">' + (data.htmlText || '') + '</textarea><br>';
+
+        $('#' + widgetID).html(text);
+
+        // subscribe on updates of value
+        if (data.oid) {
+            vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
+                $div.find('.devices-value').html(newVal);
+            });
+        }
+    }
+};
+
+vis.binds['devices'].showVersion();
