cf1b24f22ade58eac593c1118c2e83064f4a5dc6
GermanBluefox <dogafox@gmail.com>
2023-08-10 09:09:32 +0200

    Updated icons

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,16 +1,39 @@
 /**
- * Copyright 2018 bluefox <dogafox@gmail.com>
+ * Copyright 2018-2023 bluefox <dogafox@gmail.com>
  *
  * MIT License
- * 
+ *
  **/
 'use strict';
 
 const gulp       = require('gulp');
 const fs         = require('fs');
-const del        = require('del');
 
-const dir = __dirname + '/src/i18n/';
+function deleteFoldersRecursive(path, exceptions) {
+    if (fs.existsSync(path)) {
+        const stat = fs.statSync(path);
+        if (stat.isDirectory()) {
+            const files = fs.readdirSync(path);
+            for (const file of files) {
+                const curPath = `${path}/${file}`;
+                if (exceptions && exceptions.find(e => curPath.endsWith(e))) {
+                    continue;
+                }
+
+                const stat = fs.statSync(curPath);
+                if (stat.isDirectory()) {
+                    deleteFoldersRecursive(curPath);
+                    fs.rmdirSync(curPath);
+                } else {
+                    fs.unlinkSync(curPath);
+                }
+            }
+        } else {
+            fs.unlinkSync(path);
+        }
+    }
+}
+const dir = `${__dirname}/src/i18n/`;
 gulp.task('i18n=>flat', done => {
     const files = fs.readdirSync(dir).filter(name => name.match(/\.json$/));
     const index = {};
@@ -31,8 +54,8 @@ gulp.task('i18n=>flat', done => {
     const keys = Object.keys(index);
     keys.sort();
 
-    if (!fs.existsSync(dir + '/flat/')) {
-        fs.mkdirSync(dir + '/flat/');
+    if (!fs.existsSync(`${dir}/flat/`)) {
+        fs.mkdirSync(`${dir}/flat/`);
     }
 
     langs.forEach(lang => {
@@ -40,27 +63,27 @@ gulp.task('i18n=>flat', done => {
         keys.forEach(key => {
             words.push(index[key][lang]);
         });
-        fs.writeFileSync(dir + '/flat/' + lang + '.txt', words.join('\n'));
+        fs.writeFileSync(`${dir}/flat/${lang}.txt`, words.join('\n'));
     });
-    fs.writeFileSync(dir + '/flat/index.txt', keys.join('\n'));
+    fs.writeFileSync(`${dir}/flat/index.txt`, keys.join('\n'));
     done();
 });
 
 gulp.task('flat=>i18n', done => {
-    if (!fs.existsSync(dir + '/flat/')) {
-        console.error(dir + '/flat/ directory not found');
+    if (!fs.existsSync(`${dir}/flat/`)) {
+        console.error(`${dir}/flat/ directory not found`);
         return done();
     }
-    const keys = fs.readFileSync(dir + '/flat/index.txt').toString().split(/[\r\n]/);
+    const keys = fs.readFileSync(`${dir}/flat/index.txt`).toString().split(/[\r\n]/);
     while (!keys[keys.length - 1]) keys.splice(keys.length - 1, 1);
 
-    const files = fs.readdirSync(dir + '/flat/').filter(name => name.match(/\.txt$/) && name !== 'index.txt');
+    const files = fs.readdirSync(`${dir}/flat/`).filter(name => name.match(/\.txt$/) && name !== 'index.txt');
     const index = {};
     const langs = [];
     files.forEach(file => {
         const lang = file.replace(/\.txt$/, '');
         langs.push(lang);
-        const lines = fs.readFileSync(dir + '/flat/' + file).toString().split(/[\r\n]/);
+        const lines = fs.readFileSync(`${dir}/flat/${file}`).toString().split(/[\r\n]/);
         lines.forEach((word, i) => {
             index[keys[i]] = index[keys[i]] || {};
             index[keys[i]][lang] = word;
@@ -70,11 +93,11 @@ gulp.task('flat=>i18n', done => {
         const words = {};
         keys.forEach((key, line) => {
             if (!index[key]) {
-                console.log('No word ' + key + ', ' + lang + ', line: ' + line);
+                console.log(`No word ${key}, ${lang}, line: ${line}`);
             }
             words[key] = index[key][lang];
         });
-        fs.writeFileSync(dir + '/' + lang + '.json', JSON.stringify(words, null, 2));
+        fs.writeFileSync(`${dir}/${lang}.json`, JSON.stringify(words, null, 2));
     });
     done();
 });
@@ -82,15 +105,16 @@ gulp.task('flat=>i18n', done => {
 gulp.task('build', done => {
     const { fork } = require('child_process');
 
-    const child = fork(__dirname + '/node_modules/react-scripts/scripts/build.js', {
+    const child = fork(`${__dirname}/node_modules/react-scripts/scripts/build.js`, {
         cwd: __dirname
     });
     child.on('exit', (code, signal) => done());
 });
 
-gulp.task('clean', () =>
-    del(['admin/*/**', 'admin/*', '!admin/actions.js', '!admin/alexalogo.png', '!admin/blockly.js', '!admin/iot.png']));
-
+gulp.task('clean', done => {
+    deleteFoldersRecursive(`${__dirname}/admin`, ['actions.js', 'alexalogo.png', 'blockly.js', 'iot.png']);
+    done();
+});
 
 gulp.task('copy', () => {
     return gulp.src(['build/*/**', 'build/*'])
@@ -98,15 +122,15 @@ gulp.task('copy', () => {
 });
 
 gulp.task('renameIndex', gulp.series('copy', function (done) {
-    if (fs.existsSync(__dirname + '/../admin/index.html')) {
-        fs.renameSync(__dirname + '/admin/index.html', __dirname + '/admin/index_m.html')
+    if (fs.existsSync(`${__dirname}/../admin/index.html`)) {
+        fs.renameSync(`${__dirname}/admin/index.html`, __dirname + '/admin/index_m.html')
     }
     done();
 }));
 
 gulp.task('renameTab', gulp.series('copy', function (done) {
-    if (fs.existsSync(__dirname + '/../admin/index.html')) {
-        fs.renameSync(__dirname + '/../admin/index.html', __dirname + '/../admin/tab.html')
+    if (fs.existsSync(`${__dirname}/../admin/index.html`)) {
+        fs.renameSync(`${__dirname}/../admin/index.html`, `${__dirname}/../admin/tab.html`)
     }
     done();
 }));
