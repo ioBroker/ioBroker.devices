a7e3ccfbad32f638294f17fb166d808d38fb3531
Simon Fischer <simi.fischa@gmail.com>
2026-08-08 00:26:05 +0200

    build output

--- admin/assets/mf-entry-bootstrap-0-3db78b20.js
+++ admin/assets/mf-entry-bootstrap-0-3db78b20.js
@@ -33,4 +33,4 @@ const __mfImport = (src) =>
   if (__mfModuleCache.pendingShareLoads) {
     await Promise.all(__mfModuleCache.pendingShareLoads);
   }
-})().then(() => __mfImport("./index-D_8fdWt7.js"));
+})().then(() => __mfImport("./index-CzMsLsSC.js"));
